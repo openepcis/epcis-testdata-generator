ac2f867b5bbc7812495a40520cd1760b77d922f7
Sven Boeckelmann <sboeckelmann@benelog.com>
2023-12-22 00:37:07 +0100

    registered arm64 runner, now try again

--- testdata-generator-bulk/src/main/java/io/openepcis/testdata/bulk/Main.java
+++ testdata-generator-bulk/src/main/java/io/openepcis/testdata/bulk/Main.java
@@ -5,26 +5,21 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import io.openepcis.testdata.generator.EPCISEventGenerator;
-import io.openepcis.testdata.generator.identifier.instances.GenerateSGTIN;
 import io.openepcis.testdata.generator.reactivestreams.StreamingEPCISDocument;
 import io.openepcis.testdata.generator.template.InputTemplate;
-import io.openepcis.testdata.generator.template.ObjectEventType;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
 
 public class Main {
 
 
   private static final ObjectMapper MAPPER = new ObjectMapper()
           .setSerializationInclusion(JsonInclude.Include.NON_NULL)
-          .registerModule(new Jdk8Module())
           .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
           .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
           .registerModule(new JavaTimeModule());
@@ -45,15 +40,14 @@ public class Main {
             "toTime": "2023-12-20T12:37:22+01:00"
         },
         "readPoint": {
-            "gcpLength": null,
+            "gcpLength": 7,
             "gln": "9521999100003",
             "extension": "0",
             "type": "SGLN"
         },
         "referencedIdentifier": [{
             "identifierId": 1,
-            "epcCount": 4,
-            "classCount": 0
+            "epcCount": 4
         }],
         "parentReferencedIdentifier": {},
         "outputReferencedIdentifier": []
@@ -64,42 +58,29 @@ public class Main {
         "instanceData": {
             "sgtin": {
                 "identifierType": "sgtin",
-                "gcpLength": null,
+                "gcpLength": 7,
                 "sgtin": "09521987654327",
                 "serialType": "range",
                 "rangeFrom": 1,
                 "count": 4
             }
-        },
-        "classData": null,
-        "parentData": null
+        }
     }]
 }          
           """;
 
   public static void main(String[] args) throws JsonProcessingException, IOException {
-
     InputTemplate template = MAPPER.readValue(TEMPLATE, InputTemplate.class);
-    GenerateSGTIN generateSGTIN = (GenerateSGTIN) template.getIdentifiers().get(0).getInstanceData();
-    //System.out.println(generateSGTIN);
-    ObjectEventType objectEventType = (ObjectEventType) template.getEvents().get(0);
-    System.out.println(objectEventType);
     String baseBath = args[0];
-    create(template,baseBath+"/10-1", 10, 1);
-    create(template,baseBath+"/100-1", 100, 1);
-    create(template,baseBath+"/1000-1", 1000, 1);
-    create(template,baseBath+"/10-10", 10, 10);
-    create(template,baseBath+"/100-10", 100, 10);
-    create(template,baseBath+"/1000-10", 1000, 10);
-    create(template,baseBath+"/10-100", 10, 100);
-    create(template,baseBath+"/100-100", 100, 100);
-    create(template,baseBath+"/1000-100", 1000, 100);
-    create(template,baseBath+"/10-1000", 10, 1000);
-    create(template,baseBath+"/100-1000", 100, 1000);
-    create(template,baseBath+"/1000-1000", 1000, 1000);
+    int[] step = new int[] {1, 10, 100, 1000};
+    for (int eventCount : step) {
+      for (int documentCount : step) {
+        create(template,baseBath+"/"+documentCount+"-"+eventCount, documentCount, eventCount);
+      }
+    }
   }
 
-  private static void create(InputTemplate template, String dir, int count, int size) throws IOException {
+  private static void create(final InputTemplate template, final String dir, final int count, final int size) throws IOException {
     template.getEvents().get(0).setEventCount(size);
     Path d = Path.of(dir);
     if (!Files.exists(d)) {
